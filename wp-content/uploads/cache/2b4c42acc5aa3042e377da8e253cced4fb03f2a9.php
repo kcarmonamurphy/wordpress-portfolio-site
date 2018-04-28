ff8a6081ad1c058b3c97f70c6bf0a11c1b45dc39
Kevin Carmona-Murphy <kevcom@live.ca>
2018-04-28 18:44:30 -0400

    added styles pertaining to page content dawg

--- wp-content/uploads/cache/2b4c42acc5aa3042e377da8e253cced4fb03f2a9.php
+++ wp-content/uploads/cache/2b4c42acc5aa3042e377da8e253cced4fb03f2a9.php
@@ -1,7 +1,7 @@
 <article <?php (post_class()); ?>>
   <header>
-    <h1 class="entry-title"><?php echo e(get_the_title()); ?></h1>
-    <?php echo $__env->make('partials/entry-meta', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+    <?php echo $__env->make('partials.page-header', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
+    <?php echo $__env->make('partials.entry-meta', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
   </header>
   <div class="entry-content">
     <?php (the_content()); ?>
@@ -10,5 +10,5 @@
     <?php echo wp_link_pages(['echo' => 0, 'before' => '<nav class="page-nav"><p>' . __('Pages:', 'sage'), 'after' => '</p></nav>']); ?>
 
   </footer>
-  <?php (comments_template('/partials/comments.blade.php')); ?>
+  
 </article>
